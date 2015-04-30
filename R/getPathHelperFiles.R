7f2a88d93c2e51b10827e6ea3650e9c72435def8
Sonali Arora <sarora@fhcrc.org>
2015-04-30 20:26:48 +0000

    BUG FIX
    bug fix in release version based on author's request 
    
    
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/branches/RELEASE_3_1/experiment/pkgs/CopyhelpeR@3291 db2202da-8704-0410-b924-b125b3cd84ef

--- R/getPathHelperFiles.R
+++ R/getPathHelperFiles.R
@@ -1,17 +1,17 @@
 getPathHelperFiles <- function(ref.genome) {
     switch(ref.genome,
            "hg18" = dir(system.file(package = "CopyhelpeR", "extdata"),
-                        pattern = "^hg18", full.names = TRUE),
+                        pattern = "hg18_1kb", full.names = TRUE),
            "hg19" = dir(system.file(package = "CopyhelpeR", "extdata"),
-                        pattern = "^hg19", full.names = TRUE),
+                        pattern = "hg19_1kb", full.names = TRUE),
            "hg38" = dir(system.file(package = "CopyhelpeR", "extdata"),
-                        pattern = "^hg38", full.names = TRUE),
+                        pattern = "hg38_1kb", full.names = TRUE),
            "mm9" = dir(system.file(package = "CopyhelpeR", "extdata"),
-                       pattern = "^mm9", full.names = TRUE),
+                       pattern = "mm9_1kb", full.names = TRUE),
            "mm10" = dir(system.file(package = "CopyhelpeR", "extdata"),
-                        pattern = "^mm10", full.names = TRUE),
+                        pattern = "mm10_1kb$", full.names = TRUE),
            "mm10_4" = dir(system.file(package = "CopyhelpeR", "extdata"),
-                        pattern = "_4$", full.names = TRUE),
+                        pattern = "mm10_1kb_4", full.names = TRUE),
            stop(.wrap("There are currently no helper files available for",
                       "reference genome", sQuote(ref.genome))))
 }
